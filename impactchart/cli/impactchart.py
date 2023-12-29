60fe28b5a5bab08ba63bee4b3a973f15a569e582
vengroff <vengroff@gmail.com>
2023-12-29 14:46:09 +0100

    WIP: sample Makefile generating plots.

--- impactchart/cli/impactchart.py
+++ impactchart/cli/impactchart.py
@@ -13,11 +13,17 @@ from logargparser import LoggingArgumentParser
 from pathlib import Path
 
 import pandas as pd
+import numpy as np
+
+from matplotlib.ticker import FuncFormatter, PercentFormatter
+
 import xgboost
 import yaml
 from scipy import stats
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import RandomizedSearchCV
+
+from impactchart.model import XGBoostImpactModel
 
 
 logger = getLogger(__name__)
@@ -29,6 +35,8 @@ def optimize_xgb(
     y_col: str,
     w_col: Optional[str] = None,
 ) -> Dict[str, Any]:
+    logger.info("Optimizing the XBG model.")
+
     reg_xgb = xgboost.XGBRegressor()
 
     param_dist = {
@@ -67,6 +75,8 @@ def optimize_xgb(
 
     result["params"]["learning_rate"] = float(result["params"]["learning_rate"])
     result["params"]["subsample"] = float(result["params"]["subsample"])
+
+    logger.info("Optimization complete.")
 
     return result
 
@@ -135,8 +145,173 @@ def optimize(args):
             yaml.dump(params, f, sort_keys=True)
 
 
+def _linreg_from_coefficients(coef, intercept):
+    reg_linreg = LinearRegression()
+    # Instead of fitting, we are just going to kludge in
+    # the known coefficients.
+    reg_linreg.coef_ = np.array(coef)
+    reg_linreg.intercept_ = intercept
+    return reg_linreg
+
+
+def _plot_id(feature, k, n, seed):
+    return f"(f = {feature}; n = {n:,.0f}; k = {k}; s = {seed:08X})"
+
+
+def plot_impact_charts(
+    impact_model: XGBoostImpactModel,
+    X: pd.DataFrame,
+    output_path: Path,
+    k: int,
+    seed: int,
+    *,
+    linreg: bool = False,
+    linreg_coefs: Optional[Iterable[float]] = None,
+    linreg_intercept: Optional[float] = None,
+):
+    if linreg:
+        reg_linreg = _linreg_from_coefficients(linreg_coefs, linreg_intercept)
+
+    logger.info("Fitting the impact chart model.")
+    impact_charts = impact_model.impact_charts(
+        X,
+        X.columns,
+        subplots_kwargs=dict(
+            figsize=(12, 8),
+        ),
+        # feature_names=,
+        y_name="Y Name",
+        subtitle="Subtitle",
+    )
+    logger.info("Fitting complete.")
+
+    dollar_formatter = FuncFormatter(
+        lambda d, pos: f"\\${d:,.0f}" if d >= 0 else f"(\\${-d:,.0f})"
+    )
+
+    for feature, (fig, ax) in impact_charts.items():
+        logger.info(f"Plotting {feature}")
+
+        # Plot the linear line by plotting the output of the linear
+        # model with all other features zeroed out so they have no
+        # effect.
+        df_one_feature = pd.DataFrame(
+            {f: X[f] if f == feature else 0.0 for f in X.columns}
+        )
+
+        if linreg:
+            df_one_feature["impact"] = reg_linreg.predict(df_one_feature)
+
+            df_endpoints = pd.concat(
+                [
+                    df_one_feature.nsmallest(1, feature),
+                    df_one_feature.nlargest(1, feature),
+                ]
+            )
+
+            ax = df_endpoints.plot.line(
+                feature, "impact", color="orange", ax=ax, label="Linear Model"
+            )
+
+        plot_id = _plot_id(feature, k, len(X.index), seed)
+        ax.text(
+            0.99,
+            0.01,
+            plot_id,
+            fontsize=8,
+            backgroundcolor="white",
+            horizontalalignment="right",
+            verticalalignment="bottom",
+            transform=ax.transAxes,
+        )
+
+        col_is_fractional = feature.startswith("frac_")
+
+        if col_is_fractional:
+            ax.xaxis.set_major_formatter(PercentFormatter(1.0, decimals=0))
+            ax.set_xlim(-0.05, 1.05)
+        else:
+            ax.xaxis.set_major_formatter(dollar_formatter)
+            ax.set_xlim(-5_000, max(10_000, df_one_feature[feature].max()) * 1.05)
+
+        ax.grid(visible=True)
+
+        logger.info(f"Saving impact chart for {feature}.")
+        fig.savefig(output_path / f"{feature}.png")
+
+
 def plot(args):
-    pass
+    data_path = Path(args.data)
+
+    df = pd.read_csv(
+        data_path, header=0, dtype={"STATE": str, "COUNTY": str, "TRACT": str}
+    )
+
+    with open(args.parameters) as f:
+        param_file_contents = yaml.full_load(f)
+
+    xgb_params = param_file_contents["xgb"]["params"]
+
+    if args.linreg and "linreg" in param_file_contents:
+        linreg_coefs = param_file_contents["linreg"]["coefficients"]
+        linreg_intercept = param_file_contents["linreg"]["intercept"]
+    else:
+        linreg_coefs = None
+        linreg_intercept = None
+
+    X = df[args.X_columns]
+    y = df[args.y_column]
+    w = df[args.w_column]
+
+    # TODO - override k and seed with args.
+    k = 50
+    seed = 0x3423CDF1
+
+    impact_model = XGBoostImpactModel(
+        ensemble_size=k, random_state=seed, estimator_kwargs=xgb_params
+    )
+    impact_model.fit(X, y, sample_weight=w)
+
+    output_path = Path(args.output)
+
+    plot_impact_charts(
+        impact_model,
+        X,
+        output_path,
+        k,
+        seed,
+        linreg=args.linreg,
+        linreg_coefs=linreg_coefs,
+        linreg_intercept=linreg_intercept,
+    )
+
+
+def add_data_arguments(parser) -> None:
+    parser.add_argument(
+        "-X",
+        "--X-columns",
+        type=str,
+        nargs="+",
+        required=True,
+        help="X columns (features) for fitting the models.",
+    )
+
+    parser.add_argument(
+        "-y",
+        "--y-column",
+        type=str,
+        required=True,
+        help="What variable are we trying to predict?",
+    )
+
+    parser.add_argument(
+        "-w",
+        "--w-column",
+        type=str,
+        help="Weight column.",
+    )
+
+    parser.add_argument("data", help="Input data file. Typically from select.py.")
 
 
 def main():
@@ -157,41 +332,26 @@ def main():
 
     plot_parser = subparsers.add_parser("plot", help="Generate an impact chart.")
 
+    add_data_arguments(optimize_parser)
+    add_data_arguments(plot_parser)
+
     optimize_parser.add_argument(
         "-o", "--output", required=True, type=str, help="Output yaml file."
-    )
-
-    optimize_parser.add_argument(
-        "-X",
-        "--X-columns",
-        type=str,
-        nargs="+",
-        required=True,
-        help="X columns (features) for fitting the models.",
-    )
-
-    optimize_parser.add_argument(
-        "-y",
-        "--y-column",
-        type=str,
-        required=True,
-        help="What variable are we trying to predict?",
-    )
-
-    optimize_parser.add_argument(
-        "-w",
-        "--w-column",
-        type=str,
-        help="Weight column.",
-    )
-
-    optimize_parser.add_argument(
-        "data", help="Input data file. Typically from select.py."
     )
 
     plot_parser.add_argument(
         "-o", "--output", required=True, type=str, help="Output plot file."
     )
+
+    plot_parser.add_argument(
+        "-p",
+        "--parameters",
+        required=True,
+        type=str,
+        help="Model parameters (from optimize.py).",
+    )
+
+    plot_parser.add_argument("--linreg", action="store_true")
 
     args = parser.parse_args()
 
