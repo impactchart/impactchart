ec2fab6ee0655757f78386d248b7d2f38d3b2064
vengroff <vengroff@gmail.com>
2024-02-26 10:59:32 -0500

    Add images and a script to combine them into a poster.

---
+++ images/combine_images.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# This script combines some of the images from the documentation into
+# a single large image suitable for conference submissions that only
+# allow a single image.
+
+# Synthetic data, with intermediate subtitles.
+montage -tile 2x1 -title "When scatter plots are ineffective or murky..." -geometry +32+32 -border 8 y_vs_x3.png y_vs_x2.png scatter.png
+montage -tile 2x1 -title "Impact charts clearly identify the impact of a single feature." -geometry +32+32 -border 8 x3_impact.png x2_impact.png synth_impact.png
+
+#montage -tile 1x2 -geometry +32+32 scatter.png synth_impact.png synth.png
+convert scatter.png synth_impact.png -gravity south -append synth.png
+
+# Real data
+montage -tile 2x1 -title "A real world example from an analysis of the impact of income, race, and ethnicity on eviction rates." -geometry +32+32 -border 8 13089-income.png 13089-black.png real-impact.png
+
+
+# QR code
+
+convert qrcode.png -bordercolor white -border 514x10 -gravity South -pointsize 24 -annotate 0 "Scan or visit https://github.com/vengroff/impactchart to learn more." qrannotated.png
+
+# Combine them all
+
+montage -tile 1x3 -pointsize 48 -title "Introduction to Impact Charts" -geometry +32+32 synth.png real-impact.png qrannotated.png poster.png # all-plots.png
+
+# convert all-plots.png qrannotated.png -gravity south -append -annotate +0+0 XXXX poster.png
+
+
+
+
+
+
+
+
